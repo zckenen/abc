e48713dc9be0e42f0af084118de493e1a8febab2
zckenen <377491985@qq.com>
2017-06-27 09:34:36 +0800

    Add files via upload
    第一个

---
+++ Convert.java
@@ -0,0 +1,16 @@
+package solution;
+
+public class Convert {
+	public String convert(String s,int nRows)
+	{
+		char[] c=s.toCharArray();
+		int len=s.length();
+		StringBuffer sb=new StringBuffer(nRows);
+		while()
+		{
+			
+		}
+		
+	}
+
+}
